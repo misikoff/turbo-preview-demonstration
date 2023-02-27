13eb619460e953fd3270a912ae30095c59629d22
Thomas Misikoff <tmisikoff@gmail.com>
2023-02-27 15:30:52 -0500

    feat: change a again

--- apps/a/pages/index.tsx
+++ apps/a/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Home Page - A</title>
       </Head>
-      <span className='mt-8 text-xl'>This is App A-change2</span>
+      <span className='mt-8 text-xl'>This is App A-change3</span>
       <NewTabLink href='/'>this is from a shared library</NewTabLink>
     </>
   )
