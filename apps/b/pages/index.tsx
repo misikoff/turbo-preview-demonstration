08e099e60072aa380d2f2ebdda8c35eebb689d3c
Thomas Misikoff <tmisikoff@gmail.com>
2023-02-27 12:50:38 -0500

    feat: add shared components library

--- apps/b/pages/index.tsx
+++ apps/b/pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head'
+import { NewTabLink } from 'shared-components'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Home Page - B</title>
+        <title>Home Page - A</title>
       </Head>
-      <span className='mt-8 text-xl'>This is App B</span>
+      <span className='mt-8 text-xl'>This is App A</span>
+      <NewTabLink href='/'>this is from a shared library</NewTabLink>
     </>
   )
 }
