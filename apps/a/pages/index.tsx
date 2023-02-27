4c215cc3af14aa6a40d0f561e939a9e7372cef92
Thomas Misikoff <tmisikoff@gmail.com>
2023-02-27 12:31:30 -0500

    feat: initial set up

---
+++ apps/a/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home Page - A</title>
+      </Head>
+      <span className='mt-8 text-xl'>This is App A</span>
+    </>
+  )
+}
